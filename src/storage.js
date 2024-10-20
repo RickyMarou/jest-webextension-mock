ccf4f1968b75fce92585fdb522cc7476f49cae6c
Michal Stanke <michal@stanke.cz>
2024-10-20 14:15:52 +0200

    fix: `browser.storage` should not include unknown keys in the result
    As described in the [documentation](https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/storage/StorageArea/get), when the key is not present in the storage, it should be completely missing from the `.get()` result as well. The current mock implementation was returning the key in the result with `undefined` value.
    
    Also the lastly added session storage mock was sharing data with local storage.

--- src/storage.js
+++ src/storage.js
@@ -1,191 +1,81 @@
 import { createEventListeners } from './createEventListeners';
 
-let syncStore = {};
-let localStore = {};
-let managedStore = {};
-let sessionStore = {};
-
 function resolveKey(key, store) {
   if (typeof key === 'string') {
-    const result = {};
-    result[key] = store[key];
-    return result;
+    if (key in store) {
+      return { key: store[key] };
+    } else {
+      return {};
+    }
   } else if (Array.isArray(key)) {
-    return key.reduce((acc, curr) => {
-      acc[curr] = store[curr];
-      return acc;
-    }, {});
+    return key.reduce(
+      (acc, currKey) => ({
+        ...acc,
+        ...resolveKey(currKey, store),
+      }),
+      {}
+    );
   } else if (typeof key === 'object') {
-    return Object.keys(key).reduce((acc, curr) => {
-      acc[curr] = store[curr] || key[curr];
-      return acc;
-    }, {});
+    return Object.entries(key).reduce(
+      (acc, [currKey, fallbackValue]) => ({
+        ...acc,
+        [currKey]: fallbackValue,
+        ...resolveKey(currKey, store),
+      }),
+      {}
+    );
   }
   throw new Error('Wrong key given');
 }
 
+function mockStore() {
+  const store = {};
+
+  return {
+    get: jest.fn((id, cb) => {
+      const result =
+        id === null || id === undefined ? store : resolveKey(id, store);
+      if (cb !== undefined) {
+        return cb(result);
+      }
+      return Promise.resolve(result);
+    }),
+    getBytesInUse: jest.fn((id, cb) => {
+      if (cb !== undefined) {
+        return cb(0);
+      }
+      return Promise.resolve(0);
+    }),
+    set: jest.fn((payload, cb) => {
+      Object.keys(payload).forEach((key) => (store[key] = payload[key]));
+      if (cb !== undefined) {
+        return cb();
+      }
+      return Promise.resolve();
+    }),
+    remove: jest.fn((id, cb) => {
+      const keys = typeof id === 'string' ? [id] : id;
+      keys.forEach((key) => delete store[key]);
+      if (cb !== undefined) {
+        return cb();
+      }
+      return Promise.resolve();
+    }),
+    clear: jest.fn((cb) => {
+      Object.keys(store).forEach((key) => delete store[key]);
+      if (cb !== undefined) {
+        return cb();
+      }
+      return Promise.resolve();
+    }),
+    onChanged: createEventListeners(),
+  };
+}
+
 export const storage = {
-  sync: {
-    get: jest.fn((id, cb) => {
-      const result =
-        id === null || id === undefined ? syncStore : resolveKey(id, syncStore);
-      if (cb !== undefined) {
-        return cb(result);
-      }
-      return Promise.resolve(result);
-    }),
-    getBytesInUse: jest.fn((id, cb) => {
-      if (cb !== undefined) {
-        return cb(0);
-      }
-      return Promise.resolve(0);
-    }),
-    set: jest.fn((payload, cb) => {
-      Object.keys(payload).forEach((key) => (syncStore[key] = payload[key]));
-      if (cb !== undefined) {
-        return cb();
-      }
-      return Promise.resolve();
-    }),
-    remove: jest.fn((id, cb) => {
-      const keys = typeof id === 'string' ? [id] : id;
-      keys.forEach((key) => delete syncStore[key]);
-      if (cb !== undefined) {
-        return cb();
-      }
-      return Promise.resolve();
-    }),
-    clear: jest.fn((cb) => {
-      syncStore = {};
-      if (cb !== undefined) {
-        return cb();
-      }
-      return Promise.resolve();
-    }),
-    onChanged: createEventListeners(),
-  },
-  local: {
-    get: jest.fn((id, cb) => {
-      const result =
-        id === null || id === undefined
-          ? localStore
-          : resolveKey(id, localStore);
-      if (cb !== undefined) {
-        return cb(result);
-      }
-      return Promise.resolve(result);
-    }),
-    getBytesInUse: jest.fn((id, cb) => {
-      if (cb !== undefined) {
-        return cb(0);
-      }
-      return Promise.resolve(0);
-    }),
-    set: jest.fn((payload, cb) => {
-      Object.keys(payload).forEach((key) => (localStore[key] = payload[key]));
-      if (cb !== undefined) {
-        return cb();
-      }
-      return Promise.resolve();
-    }),
-    remove: jest.fn((id, cb) => {
-      const keys = typeof id === 'string' ? [id] : id;
-      keys.forEach((key) => delete localStore[key]);
-      if (cb !== undefined) {
-        return cb();
-      }
-      return Promise.resolve();
-    }),
-    clear: jest.fn((cb) => {
-      localStore = {};
-      if (cb !== undefined) {
-        return cb();
-      }
-      return Promise.resolve();
-    }),
-    onChanged: createEventListeners(),
-  },
-  session: {
-    get: jest.fn((id, cb) => {
-      const result =
-        id === null || id === undefined
-          ? localStore
-          : resolveKey(id, localStore);
-      if (cb !== undefined) {
-        return cb(result);
-      }
-      return Promise.resolve(result);
-    }),
-    getBytesInUse: jest.fn((id, cb) => {
-      if (cb !== undefined) {
-        return cb(0);
-      }
-      return Promise.resolve(0);
-    }),
-    set: jest.fn((payload, cb) => {
-      Object.keys(payload).forEach((key) => (localStore[key] = payload[key]));
-      if (cb !== undefined) {
-        return cb();
-      }
-      return Promise.resolve();
-    }),
-    remove: jest.fn((id, cb) => {
-      const keys = typeof id === 'string' ? [id] : id;
-      keys.forEach((key) => delete localStore[key]);
-      if (cb !== undefined) {
-        return cb();
-      }
-      return Promise.resolve();
-    }),
-    clear: jest.fn((cb) => {
-      localStore = {};
-      if (cb !== undefined) {
-        return cb();
-      }
-      return Promise.resolve();
-    }),
-    onChanged: createEventListeners(),
-  },
-  managed: {
-    get: jest.fn((id, cb) => {
-      const result =
-        id === null || id === undefined
-          ? managedStore
-          : resolveKey(id, managedStore);
-      if (cb !== undefined) {
-        return cb(result);
-      }
-      return Promise.resolve(result);
-    }),
-    getBytesInUse: jest.fn((id, cb) => {
-      if (cb !== undefined) {
-        return cb(0);
-      }
-      return Promise.resolve(0);
-    }),
-    set: jest.fn((payload, cb) => {
-      Object.keys(payload).forEach((key) => (managedStore[key] = payload[key]));
-      if (cb !== undefined) {
-        return cb();
-      }
-      return Promise.resolve();
-    }),
-    remove: jest.fn((id, cb) => {
-      const keys = typeof id === 'string' ? [id] : id;
-      keys.forEach((key) => delete managedStore[key]);
-      if (cb !== undefined) {
-        return cb();
-      }
-      return Promise.resolve();
-    }),
-    clear: jest.fn((cb) => {
-      managedStore = {};
-      if (cb !== undefined) {
-        return cb();
-      }
-      return Promise.resolve();
-    }),
-    onChanged: createEventListeners(),
-  },
+  sync: mockStore(),
+  local: mockStore(),
+  session: mockStore(),
+  managed: mockStore(),
   onChanged: createEventListeners(),
 };
